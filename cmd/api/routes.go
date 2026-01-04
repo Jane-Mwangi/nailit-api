a8814f03baf65e638663316e2cea25149b1459d1
Jane-Mwangi <janiegrace097@gmail.com>
2026-01-04 19:58:48 +0300

    feat: implement create service type endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,13 +12,16 @@ func (app *application) routes() *httprouter.Router {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-	// Health check route
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	// Services
 	router.HandlerFunc(http.MethodGet, "/v1/services", app.getAllServicesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/services", app.createServiceHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/services/:id", app.getServiceHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/services/:id", app.updateServiceHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/services/:id", app.deleteServiceHandler)
 
+	// ServiceTypes
+	router.HandlerFunc(http.MethodPost, "/v1/service-types", app.createServiceTypesHandler)
 	return router
 }
--- internal/data/models.go
+++ internal/data/models.go
@@ -6,17 +6,20 @@ import (
 )
 
 var (
-	ErrRecordNotFound   = errors.New("record not found")
-	ErrEditConflict     = errors.New("edit conflict")
-	ErrDuplicateService = errors.New("service with this name already exists")
+	ErrRecordNotFound       = errors.New("record not found")
+	ErrEditConflict         = errors.New("edit conflict")
+	ErrDuplicateService     = errors.New("service with this name already exists")
+	ErrDuplicateServiceType = errors.New("duplicate service type")
 )
 
 type Models struct {
-	Services *ServiceModel
+	Services     *ServiceModel
+	ServiceTypes *ServiceTypesModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Services: &ServiceModel{DB: db},
+		Services:     &ServiceModel{DB: db},
+		ServiceTypes: &ServiceTypesModel{DB: db},
 	}
 }
