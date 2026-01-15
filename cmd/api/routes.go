62fe72d5a64e184714385762528b259272b2d1c7
Jane-Mwangi <janiegrace097@gmail.com>
2026-01-15 22:10:03 +0300

    feat: require only activated users to access resources

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,25 +15,25 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	// Services
-	router.HandlerFunc(http.MethodGet, "/v1/services", app.getAllServicesHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/services", app.createServiceHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/services/:id", app.getServiceHandler)
-	router.HandlerFunc(http.MethodPut, "/v1/services/:id", app.updateServiceHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/services/:id", app.deleteServiceHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/services", app.requireActivatedUser(app.getAllServicesHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/services", app.requireActivatedUser(app.createServiceHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/services/:id", app.requireActivatedUser(app.getServiceHandler))
+	router.HandlerFunc(http.MethodPut, "/v1/services/:id", app.requireActivatedUser(app.updateServiceHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/services/:id", app.requireActivatedUser(app.deleteServiceHandler))
 
 	// ServiceTypes
-	router.HandlerFunc(http.MethodPost, "/v1/service-types", app.createServiceTypesHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/service-types/:id", app.getServiceTypeHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/service-types", app.getAllServiceTypesHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/service-types/:id", app.updateServiceTypeHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/service-types/:id", app.deleteServiceTypeHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/service-types", app.requireActivatedUser(app.createServiceTypesHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/service-types/:id", app.requireActivatedUser(app.getServiceTypeHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/service-types", app.requireActivatedUser(app.getAllServiceTypesHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/service-types/:id", app.requireActivatedUser(app.updateServiceTypeHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/service-types/:id", app.requireActivatedUser(app.deleteServiceTypeHandler))
 
 	// Staff
-	router.HandlerFunc(http.MethodPost, "/v1/staff", app.createStaffHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/staff/:id", app.getStaffByIdHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/staff", app.getAllStaffHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/staff/:id", app.updateStaffHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/staff/:id", app.deleteStaffHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/staff", app.requireActivatedUser(app.createStaffHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/staff/:id", app.requireActivatedUser(app.getStaffByIdHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/staff", app.requireActivatedUser(app.getAllStaffHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/staff/:id", app.requireActivatedUser(app.updateStaffHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/staff/:id", app.requireActivatedUser(app.deleteStaffHandler))
 
 	// users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
