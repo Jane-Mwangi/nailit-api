75d989aa84c6c1e3d5cbb8dcfc82c7d2c9a3db81
Jane-Mwangi <janiegrace097@gmail.com>
2026-01-16 23:48:09 +0300

    feat: implement permission based authorization

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,25 +15,25 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	// Services
-	router.HandlerFunc(http.MethodGet, "/v1/services", app.requireActivatedUser(app.getAllServicesHandler))
-	router.HandlerFunc(http.MethodPost, "/v1/services", app.requireActivatedUser(app.createServiceHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/services/:id", app.requireActivatedUser(app.getServiceHandler))
-	router.HandlerFunc(http.MethodPut, "/v1/services/:id", app.requireActivatedUser(app.updateServiceHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/services/:id", app.requireActivatedUser(app.deleteServiceHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/services", app.requirePermission("movies:read", app.getAllServicesHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/services", app.requirePermission("movies:write", app.createServiceHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/services/:id", app.requirePermission("movies:read", app.getServiceHandler))
+	router.HandlerFunc(http.MethodPut, "/v1/services/:id", app.requirePermission("movies:write", app.updateServiceHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/services/:id", app.requirePermission("movies:write", app.deleteServiceHandler))
 
 	// ServiceTypes
-	router.HandlerFunc(http.MethodPost, "/v1/service-types", app.requireActivatedUser(app.createServiceTypesHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/service-types/:id", app.requireActivatedUser(app.getServiceTypeHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/service-types", app.requireActivatedUser(app.getAllServiceTypesHandler))
-	router.HandlerFunc(http.MethodPatch, "/v1/service-types/:id", app.requireActivatedUser(app.updateServiceTypeHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/service-types/:id", app.requireActivatedUser(app.deleteServiceTypeHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/service-types", app.requirePermission("movies:write", app.createServiceTypesHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/service-types/:id", app.requirePermission("movies:read", app.getServiceTypeHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/service-types", app.requirePermission("movies:read", app.getAllServiceTypesHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/service-types/:id", app.requirePermission("movies:write", app.updateServiceTypeHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/service-types/:id", app.requirePermission("movies:write", app.deleteServiceTypeHandler))
 
 	// Staff
-	router.HandlerFunc(http.MethodPost, "/v1/staff", app.requireActivatedUser(app.createStaffHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/staff/:id", app.requireActivatedUser(app.getStaffByIdHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/staff", app.requireActivatedUser(app.getAllStaffHandler))
-	router.HandlerFunc(http.MethodPatch, "/v1/staff/:id", app.requireActivatedUser(app.updateStaffHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/staff/:id", app.requireActivatedUser(app.deleteStaffHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/staff", app.requirePermission("movies:write", app.createStaffHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/staff/:id", app.requirePermission("movies:read", app.getStaffByIdHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/staff", app.requirePermission("movies:read", app.getAllStaffHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/staff/:id", app.requirePermission("movies:write", app.updateStaffHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/staff/:id", app.requirePermission("movies:write", app.deleteStaffHandler))
 
 	// users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
